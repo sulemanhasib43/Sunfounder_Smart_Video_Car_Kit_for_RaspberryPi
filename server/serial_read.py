30090c4e56eca79a4499fe42fdead1590d525cf4
Suleman Hasib <sys.admin@vantibolli.com>
2018-03-30 11:58:46 +0500

    split of array added

--- server/serial_read.py
+++ server/serial_read.py
@@ -17,4 +17,8 @@ counter = 0
 while 1:
         x = ser.readline()
         if "GPGGA" in x:
-            print x
+            #print x
+            arr = x.split(',', 7);
+            i = 2;
+            while i < 8:
+            print(arr[i]) i += 1
