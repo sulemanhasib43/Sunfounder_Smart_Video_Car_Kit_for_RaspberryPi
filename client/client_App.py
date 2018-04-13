1937bceecfa125acae166b068b90c8edf08d91e2
Suleman Hasib <sys.admin@vantibolli.com>
2018-04-13 14:39:57 +0500

    serial update

--- client/client_App.py
+++ client/client_App.py
@@ -1,77 +1,91 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from Tkinter import *
-from socket import *      # Import necessary modules
+from socket import *  # Import necessary modules
 
-ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
+ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-',
+            'xy_home']
 
-top = Tk()   # Create a top window
+top = Tk()  # Create a top window
 top.title('Sunfounder Raspberry Pi Smart Video Car')
 
-HOST = '192.168.0.147'    # Server(Raspberry Pi) IP address
+HOST = '192.168.0.147'  # Server(Raspberry Pi) IP address
 PORT = 21567
-BUFSIZ = 1024             # buffer size
+BUFSIZ = 1024  # buffer size
 ADDR = (HOST, PORT)
 
-tcpCliSock = socket(AF_INET, SOCK_STREAM)   # Create a socket
-tcpCliSock.connect(ADDR)                    # Connect with the server
+tcpCliSock = socket(AF_INET, SOCK_STREAM)  # Create a socket
+tcpCliSock.connect(ADDR)  # Connect with the server
+
 
 # =============================================================================
 # The function is to send the command forward to the server, so as to make the 
 # car move forward.
 # ============================================================================= 
 def forward_fun(event):
-	print 'forward'
-	tcpCliSock.send('forward')
+    print 'forward'
+    tcpCliSock.send('forward')
+
 
 def backward_fun(event):
-	print 'backward'
-	tcpCliSock.send('backward')
+    print 'backward'
+    tcpCliSock.send('backward')
+
 
 def left_fun(event):
-	print 'left'
-	tcpCliSock.send('left')
+    print 'left'
+    tcpCliSock.send('left')
+
 
 def right_fun(event):
-	print 'right'
-	tcpCliSock.send('right')
+    print 'right'
+    tcpCliSock.send('right')
+
 
 def stop_fun(event):
-	print 'stop'
-	tcpCliSock.send('stop')
+    print 'stop'
+    tcpCliSock.send('stop')
+
 
 def home_fun(event):
-	print 'home'
-	tcpCliSock.send('home')
+    print 'home'
+    tcpCliSock.send('home')
+
 
 def x_increase(event):
-	print 'x+'
-	tcpCliSock.send('x+')
+    print 'x+'
+    tcpCliSock.send('x+')
+
 
 def x_decrease(event):
-	print 'x-'
-	tcpCliSock.send('x-')
+    print 'x-'
+    tcpCliSock.send('x-')
+
 
 def y_increase(event):
-	print 'y+'
-	tcpCliSock.send('y+')
+    print 'y+'
+    tcpCliSock.send('y+')
+
 
 def y_decrease(event):
-	print 'y-'
-	tcpCliSock.send('y-')
+    print 'y-'
+    tcpCliSock.send('y-')
+
 
 def xy_home(event):
-	print 'xy_home'
-	tcpCliSock.send('xy_home')
+    print 'xy_home'
+    tcpCliSock.send('xy_home')
+
 
 # =============================================================================
 # Exit the GUI program and close the network connection between the client 
 # and server.
 # =============================================================================
 def quit_fun(event):
-	top.quit()
-	tcpCliSock.send('stop')
-	tcpCliSock.close()
+    top.quit()
+    tcpCliSock.send('stop')
+    tcpCliSock.close()
+
 
 # =============================================================================
 # Create buttons
@@ -86,12 +100,12 @@ Btn5 = Button(top, width=5, height=2, text='Home')
 # =============================================================================
 # Buttons layout
 # =============================================================================
-Btn0.grid(row=0,column=1)
-Btn1.grid(row=2,column=1)
-Btn2.grid(row=1,column=0)
-Btn3.grid(row=1,column=2)
-Btn4.grid(row=3,column=2)
-Btn5.grid(row=1,column=1)
+Btn0.grid(row=0, column=1)
+Btn1.grid(row=2, column=1)
+Btn2.grid(row=1, column=0)
+Btn3.grid(row=1, column=2)
+Btn4.grid(row=3, column=2)
+Btn5.grid(row=1, column=1)
 
 # =============================================================================
 # Bind the buttons with the corresponding callback function.
@@ -100,7 +114,7 @@ Btn0.bind('<ButtonPress-1>', forward_fun)  # When button0 is pressed down, call 
 Btn1.bind('<ButtonPress-1>', backward_fun)
 Btn2.bind('<ButtonPress-1>', left_fun)
 Btn3.bind('<ButtonPress-1>', right_fun)
-Btn0.bind('<ButtonRelease-1>', stop_fun)   # When button0 is released, call the function stop_fun().
+Btn0.bind('<ButtonRelease-1>', stop_fun)  # When button0 is released, call the function stop_fun().
 Btn1.bind('<ButtonRelease-1>', stop_fun)
 Btn2.bind('<ButtonRelease-1>', stop_fun)
 Btn3.bind('<ButtonRelease-1>', stop_fun)
@@ -119,11 +133,11 @@ Btn11 = Button(top, width=5, height=2, text='HOME', bg='red')
 # =============================================================================
 # Buttons layout
 # =============================================================================
-Btn07.grid(row=1,column=5)
-Btn08.grid(row=1,column=3)
-Btn09.grid(row=2,column=4)
-Btn10.grid(row=0,column=4)
-Btn11.grid(row=1,column=4)
+Btn07.grid(row=1, column=5)
+Btn08.grid(row=1, column=3)
+Btn09.grid(row=2, column=4)
+Btn10.grid(row=0, column=4)
+Btn11.grid(row=1, column=4)
 
 # =============================================================================
 # Bind button events
@@ -133,46 +147,49 @@ Btn08.bind('<ButtonPress-1>', x_decrease)
 Btn09.bind('<ButtonPress-1>', y_decrease)
 Btn10.bind('<ButtonPress-1>', y_increase)
 Btn11.bind('<ButtonPress-1>', xy_home)
-#Btn07.bind('<ButtonRelease-1>', home_fun)
-#Btn08.bind('<ButtonRelease-1>', home_fun)
-#Btn09.bind('<ButtonRelease-1>', home_fun)
-#Btn10.bind('<ButtonRelease-1>', home_fun)
-#Btn11.bind('<ButtonRelease-1>', home_fun)
+# Btn07.bind('<ButtonRelease-1>', home_fun)
+# Btn08.bind('<ButtonRelease-1>', home_fun)
+# Btn09.bind('<ButtonRelease-1>', home_fun)
+# Btn10.bind('<ButtonRelease-1>', home_fun)
+# Btn11.bind('<ButtonRelease-1>', home_fun)
 
 # =============================================================================
 # Bind buttons on the keyboard with the corresponding callback function to 
 # control the car remotely with the keyboard.
 # =============================================================================
-top.bind('<KeyPress-a>', left_fun)   # Press down key 'A' on the keyboard and the car will turn left.
-top.bind('<KeyPress-d>', right_fun) 
+top.bind('<KeyPress-a>', left_fun)  # Press down key 'A' on the keyboard and the car will turn left.
+top.bind('<KeyPress-d>', right_fun)
 top.bind('<KeyPress-s>', backward_fun)
 top.bind('<KeyPress-w>', forward_fun)
 top.bind('<KeyPress-h>', home_fun)
-top.bind('<KeyRelease-a>', home_fun) # Release key 'A' and the car will turn back.
+top.bind('<KeyRelease-a>', home_fun)  # Release key 'A' and the car will turn back.
 top.bind('<KeyRelease-d>', home_fun)
 top.bind('<KeyRelease-s>', stop_fun)
 top.bind('<KeyRelease-w>', stop_fun)
 
 spd = 50
 
+
 def changeSpeed(ev=None):
-	tmp = 'speed'
-	global spd
-	spd = speed.get()
-	data = tmp + str(spd)  # Change the integers into strings and combine them with the string 'speed'. 
-	print 'sendData = %s' % data
-	tcpCliSock.send(data)  # Send the speed data to the server(Raspberry Pi)
+    tmp = 'speed'
+    global spd
+    spd = speed.get()
+    data = tmp + str(spd)  # Change the integers into strings and combine them with the string 'speed'.
+    print 'sendData = %s' % data
+    tcpCliSock.send(data)  # Send the speed data to the server(Raspberry Pi)
+
 
 label = Label(top, text='Speed:', fg='red')  # Create a label
-label.grid(row=6, column=0)                  # Label layout
+label.grid(row=6, column=0)  # Label layout
 
 speed = Scale(top, from_=0, to=100, orient=HORIZONTAL, command=changeSpeed)  # Create a scale
 speed.set(50)
 speed.grid(row=6, column=1)
 
+
 def main():
-	top.mainloop()
+    top.mainloop()
+
 
 if __name__ == '__main__':
-	main()
-
+    main()
--- server/serial_read.py
+++ server/serial_read.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
 
 
+# GGA - essential fix data which provide 3D location and accuracy data.
+
+#  $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
+
+# Where:
+#      GGA          Global Positioning System Fix Data
+#      123519       Fix taken at 12:35:19 UTC
+#      4807.038,N   Latitude 48 deg 07.038' N
+#      01131.000,E  Longitude 11 deg 31.000' E
+#      1            Fix quality: 0 = invalid
+#                                1 = GPS fix (SPS)
+#                                2 = DGPS fix
+#                                3 = PPS fix
+#                                4 = Real Time Kinematic
+#                                5 = Float RTK
+#                                6 = estimated (dead reckoning) (2.3 feature)
+#                                7 = Manual input mode
+#                                8 = Simulation mode
+#      08           Number of satellites being tracked
+#      0.9          Horizontal dilution of position
+#      545.4,M      Altitude, Meters, above mean sea level
+#      46.9,M       Height of geoid (mean sea level) above WGS84
+#                       ellipsoid
+#      (empty field) time in seconds since last DGPS update
+#      (empty field) DGPS station ID number
+#      *47          the checksum data, always begins with *
+
+
+
 import time
 import serial
 
@@ -18,8 +47,21 @@ while 1:
         x = ser.readline()
         if "GPGGA" in x:
             #print x
-            arr = x.split(',', 7);
+            arr = x.split(',', 2);
             i = 2;
             while i < 8:
                 print (arr[i])
                 i += 1
+
+
+
+# #!/usr/bin/python
+
+# str = "1,2,3,4,5,6,7,8,9,8,11,12,13";
+# arr = str.split(',', 7);
+# i = 2;
+# while i < 8:
+#     print(arr[i])
+#     i += 1
+
+
